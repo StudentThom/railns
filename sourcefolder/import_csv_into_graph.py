92cb69214ae512eba1dd24e379fd6cddb1b76f40
Rick van Bork <rick-627@hotmail.com>
2017-11-01 14:57:56 +0000

    Tellen van kritieke lijnen

--- sourcefolder/import_csv_into_graph.py
+++ sourcefolder/import_csv_into_graph.py
@@ -27,17 +27,56 @@ with open('ConnectiesHolland.csv', newline='') as csvfile:
     for row in stations:
         g.add_edge(row[0],row[1],int(row[2]))
 
+critical_lines = 0
+
 # print graph
+# w, v are entire vertex object
+# wid, vid are the names of the vertices
 for v in g:
-        for w in v.get_connections():
-            vid = v.get_id()
-            wid = w.get_id()
-            # check whether connection is critical
-            if vid in critical_stations or wid in critical_stations:
-                print('( %s , %s, %3d), CRITICAL'  % ( vid, wid, v.get_weight(w)))
-            else:
-                print('( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w)))
+    for w in v.get_connections():
+        vid = v.get_id()
+        wid = w.get_id()
+        print(w)
+        print(wid)
+
+        # check wether station name is critical
+        if vid in critical_stations:
+
+            # for every neighbour, add 1 critical line
+            for n in g.get_vertex(vid).get_connections():
+                critical_lines += 1
+            print('( %s , %s, %3d), CRITICAL'  % ( vid, wid, v.get_weight(w)))
+
+        # check wether station name is critical
+        elif wid in critical_stations:
+            # for every neighbour, add 1 critical line
+            for n in g.get_vertex(wid).get_connections():
+                critical_lines += 1
+            print('( %s , %s, %3d), CRITICAL'  % ( vid, wid, v.get_weight(w)))
+
+        else:
+            print('( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w)))
 
 # print connections
 for v in g:
     print('g.vert_dict[%s]=%s' %(v.get_id(), g.vert_dict[v.get_id()]))
+
+count = 0
+vert_dict = {}
+
+# loop through vertices
+for v in g:
+    # get name element
+    from_id = v.get_id()
+
+    # check if name is in critical_stations
+    if station_name in critical_stations:
+
+        # get name element of all connections
+        for w in v.get_connections():
+            to_id = w.get_id()
+
+        print("TODO")
+
+    else:
+        count += 1

f91e63e23e40d0afce4f7ca3e24eacb174747570
Rick van Bork <rick-627@hotmail.com>
2017-11-01 11:40:03 +0000

    TODO

--- sourcefolder/import_csv_into_graph.py
+++ sourcefolder/import_csv_into_graph.py
@@ -8,20 +8,19 @@ import csv
 
 g = make_graph.Graph()
 
-
 # read in stations from csv and put each station as node in graph
 with open('StationsHolland.csv', newline='') as csvfile:
     stations = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in stations:
         g.add_vertex(row[0])
-        
+
 # add connections between nodes (=stations) into graph
 with open('ConnectiesHolland.csv', newline='') as csvfile:
     stations = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in stations:
         g.add_edge(row[0],row[1],int(row[2]))
-        
-        
+
+
 # print graph
 for v in g:
         for w in v.get_connections():
@@ -31,6 +30,3 @@ for v in g:
 
 for v in g:
     print('g.vert_dict[%s]=%s' %(v.get_id(), g.vert_dict[v.get_id()]))
-        
-print(g)
-

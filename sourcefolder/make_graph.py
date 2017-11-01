a69ae508b4eb8ff375723d699833f34109482cb2
Rick van Bork <rick-627@hotmail.com>
2017-11-01 10:21:58 +0000

    Comments bij Graph & Vertex class, txt bestand om functie uitleg in te geven, main in make_graph.py deleted

--- sourcefolder/make_graph.py
+++ sourcefolder/make_graph.py
@@ -6,37 +6,66 @@ Created on Oct 31, 2017
 retrieved from http://www.bogotobogo.com/python/python_graph_data_structures.php
 '''
 
+"""
+Vertex class.
+
+Initializes a vertex class.
+"""
 class Vertex:
+
+    # initialise variables
     def __init__(self, node):
         self.id = node
         self.adjacent = {}
 
+    # print initializer, pretty print statement (try printing a vertex!)
+    # printing a Vertex('node') "prints node adjacent: []"
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
 
+    # adds neighbour to the neighbour list
+    # adds them as an integer weight!
+    # add new neighbour with its weight
     def add_neighbor(self, neighbor, weight=0):
         self.adjacent[neighbor] = weight
 
+    # returns list of pointers to neighbouring vertex class objects
     def get_connections(self):
-        return self.adjacent.keys()  
+        return self.adjacent.keys()
 
+    # returns the name of the node nodename.get_id()
     def get_id(self):
         return self.id
 
+    # retrieves the weight of a vertex
     def get_weight(self, neighbor):
         return self.adjacent[neighbor]
 
+"""
+Graph class.
+
+Initializes a graph class.
+Stores all vertex names, and the number of verteces.
+"""
 class Graph:
+    # initialise graph variables
     def __init__(self):
         self.vert_dict = {}
         self.num_vertices = 0
 
+    # ???
     def __iter__(self):
         return iter(self.vert_dict.values())
 
+    # adds vertexes
     def add_vertex(self, node):
-        self.num_vertices = self.num_vertices + 1
+        self.num_vertices += 1
+
+        # make new vertex
+        # Vertex(node) initialises new vertex
         new_vertex = Vertex(node)
+
+        # add new vertex to dict
         self.vert_dict[node] = new_vertex
         return new_vertex
 
@@ -46,49 +75,23 @@ class Graph:
         else:
             return None
 
+    # add edge
     def add_edge(self, frm, to, cost = 0):
+
+        # if from is new vertex, add to dict
         if frm not in self.vert_dict:
             self.add_vertex(frm)
+
+        # if to is new vertex, add to dict
         if to not in self.vert_dict:
             self.add_vertex(to)
 
+        # in Vertex class frm, add neighbour Vertex class to, with weight cost
         self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+
+        # in Vertex class to, add neighbour Vertex class frm, with weight cost
         self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
 
+    # returns a list of all vertices
     def get_vertices(self):
         return self.vert_dict.keys()
-
-if __name__ == '__main__':
-
-    g = Graph()
-
-    g.add_vertex('a')
-    g.add_vertex('b')
-    g.add_vertex('c')
-    g.add_vertex('d')
-    g.add_vertex('e')
-    g.add_vertex('f')
-
-    g.add_edge('a', 'b', 7)  
-    g.add_edge('a', 'c', 9)
-    g.add_edge('a', 'f', 14)
-    g.add_edge('b', 'c', 10)
-    g.add_edge('b', 'd', 15)
-    g.add_edge('c', 'd', 11)
-    g.add_edge('c', 'f', 2)
-    g.add_edge('d', 'e', 6)
-    g.add_edge('e', 'f', 9)
-
-    for v in g:
-        for w in v.get_connections():
-            vid = v.get_id()
-            wid = w.get_id()
-            print('( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w)))
-
-    for v in g:
-        print('g.vert_dict[%s]=%s' %(v.get_id(), g.vert_dict[v.get_id()]))
-        
-    print(g)
-        
-        
-        

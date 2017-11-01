2468ca6bf77e31cdc4bb055ad94b0207c02cd2f6
thom <thom@laptop.com>
2017-11-01 11:58:30 +0100

    add critical property outside hraph structue

--- sourcefolder/import_csv_into_graph.py
+++ sourcefolder/import_csv_into_graph.py
@@ -18,23 +18,19 @@ with open('StationsHolland.csv', newline='') as csvfile:
 # put critical stations into one array
 with open('StationsHolland.csv', newline='') as csvfile:
     stations = csv.reader(csvfile, delimiter=',', quotechar='|')
-    kritieke_stations = []
+    critical_stations = []
     
     for row in stations:
         #print(', '.join(row))
         if row[3] == "Kritiek":
             print(row[0])
-            kritieke_stations.append(row[0])
+            critical_stations.append(row[0])
         
 # add connections between nodes (=stations) into graph
 with open('ConnectiesHolland.csv', newline='') as csvfile:
     stations = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in stations:
-        # check whether connection is critical
-        if row[0] in kritieke_stations or row[1] in kritieke_stations:
-            g.add_edge(row[0],row[1],int(row[2]))
-        else:
-            g.add_edge(row[0],row[1],int(row[2]))
+        g.add_edge(row[0],row[1],int(row[2]))
         
         
 # print graph
@@ -42,7 +38,11 @@ for v in g:
         for w in v.get_connections():
             vid = v.get_id()
             wid = w.get_id()
-            print('( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w)))
+            # check whether connection is critical
+            if vid in critical_stations or wid in critical_stations:
+                print('( %s , %s, %3d), CRITICAL'  % ( vid, wid, v.get_weight(w)))
+            else:
+                print('( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w)))
 
 for v in g:
     print('g.vert_dict[%s]=%s' %(v.get_id(), g.vert_dict[v.get_id()]))
